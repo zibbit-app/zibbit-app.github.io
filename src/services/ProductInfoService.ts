b05172785fd8105c3f000563166383110dbd3ad8
evan <evandsilverstein@gmail.com>
2022-01-29 23:25:11 -0500

    updated installer and images

--- src/services/ProductInfoService.ts
+++ src/services/ProductInfoService.ts
@@ -104,7 +104,7 @@ export class ProductInfoService {
     private initializeInstallers(): Installer[]{
       
       var title = 'zibbit';
-      var installer1 = this.getInstaller(title, '1.0.10.0'); //new Installer(title + "_v" + version, version, url);
+      var installer1 = this.getInstaller(title, '1.0.11.0'); //new Installer(title + "_v" + version, version, url);
 
       //var installer1 = this.getInstaller(title, '1.0.7.0'); //new Installer(title + "_v" + version, version, url);
 
